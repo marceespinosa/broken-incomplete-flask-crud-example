69d4471cc8ddd9135fd7a241885ef6874632099b
Vi-QAN <supertrikas123@gmail.com>
2022-12-08 15:23:59 +0000

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ CORS(app)
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'student'
-app.config['MYSQL_HOST'] = '35.241.152.235'
+app.config['MYSQL_HOST'] = '35.189.192.101'
 mysql.init_app(app)
 
 @app.route("/add") #Add Student
